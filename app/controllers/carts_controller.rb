f085c61cd904ca6cba6a08661a59def38370f166
Rodrigo Ruas <rodrigoruas2@gmail.com>
2018-08-20 19:34:55 -0300

    Finally Fixed

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,11 +9,15 @@ class CartsController < ApplicationController
     @cart = Cart.new
     @orders = Order.where(user: current_user)
     @cart = Cart.create(cart_params)
+    @orders.each do |order|
+      order.cart_id = @cart.id
+      order.save
+    end
     if @cart.save
-      @orders.each do |order|
-        order.cart_id = @cart.id
-        order.save
-      end
+      # @orders.each do |order|
+      #   order.cart = @cart
+      #   order.save
+      # end
       redirect_to cart_path(@cart)
     else
       redirect_to meals_path
@@ -23,6 +27,6 @@ class CartsController < ApplicationController
   private
 
   def cart_params
-    params.require(:cart).permit(:address, :orders => [])
+    params.require(:cart).permit(:address, :order_ids => [])
   end
 end
